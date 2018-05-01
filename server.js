cc61e2490527f4600eee6a18a432e18e09699117
Cruise Brantley <cruise.brantley@gmail.com>
2018-05-01 15:25:04 -0500

    second day of  instruction

--- server.js
+++ server.js
@@ -1,9 +1,76 @@
 const express = require("express");
+const helmet = require("helmet"); // 1 yarn add helment || npm i helmet // 2
 
 // import db from './data/db';
 const db = require("./data/db");
 
 const server = express();
+
+// add middleware
+server.use(helmet()); // 3
+server.use(express.json());
+
+// route handlers
+server.post("/api/users", (req, res) => {
+  const userInformation = req.body;
+  console.log("user information", userInformation);
+
+  db
+    .insert(userInformation)
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(err => {
+      if (err.errno === 19) {
+        res.status(400).json({ msg: "Please provide all required fields" });
+      } else {
+        res.status(500).json({ erro: err });
+      }
+    });
+});
+
+// http://foo.com?search=bar&sort=asc
+// req.query === { search: 'bar', sort: 'asc' }
+
+// http://localhost:5000/api/users?id=1 // just to use req.query
+// write it using an URL parameter instead /api/users/:id
+
+server.delete("/api/users", function(req, res) {
+  const { id } = req.query;
+  let user;
+  db
+    .findById(id)
+    .then(foundUser => {
+      user = { ...foundUser[0] };
+
+      db.remove(id).then(response => {
+        res.status(200).json(user);
+      });
+    })
+    .catch(err => {
+      res.status(500).json({ erro: err });
+    });
+});
+
+server.put("/api/users/:id", function(req, res) {
+  const { id } = req.params;
+  const update = req.body;
+
+  db
+    .update(id, update)
+    .then(count => {
+      if (count > 0) {
+        db.findById(id).then(users => {
+          res.status(200).json(users[0]);
+        });
+      } else {
+        res.status(404).json({ msg: "user not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 server.get("/", (req, res) => {
   res.send("Api running");
